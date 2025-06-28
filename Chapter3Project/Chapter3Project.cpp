eea766fb701b546815a4ae3ab2704d7bde8c9b55
HaniyahS <lionroar87453@gmail.com>
2025-06-28 18:26:17 -0400

    added second task

--- Chapter3Project/Chapter3Project.cpp
+++ Chapter3Project/Chapter3Project.cpp
@@ -5,26 +5,19 @@
 
 int main()
 {
-    //Printing out prompt
-    std::cout << "A cookie recipe calls for the following ingredients: 1.5 cups of sugar, 1 cup of butter, 2.75 cups of flour \n";
-    std:: cout << "The recipe produces 48 cookies with this amount of ingredients.\n";
-    
-    //Declaring and assigning values to variables
-    double flour, butter, sugar, x, xflour, xbutter, xsugar;
-    flour = 2.75;
-    butter = 1;
-    sugar = 1.5;
+	//Assigning random numbers
+	int num1 = std::rand();
+	int num2 = std::rand();
 
-    //Asking user how many cookies they want to make and assigning input to value
-    std::cout << "How many cookies do you wish to bake? ";
-    std::cin >> x;
+	//Printing out problem
+	std::cout << num1 << " + " << num2;
 
-    //Calculation
-    xflour = flour * (x / 48);
-    xbutter = butter * (x / 48);
-    xsugar = sugar * (x / 48);
+	//Requiring user to press enter before continuing
+	std:: cout << "\nPress \"Enter\" to see solution";
+	std:: cin.get();
 
-    //Print out results
-    std::cout << "For " << x << " cookies, you need: \n" << xsugar << " cup(s) of sugar\n" << xbutter << " cup(s) of butter\n" << xflour << " cup(s) of flour";
+	int result;
+	result = num1 + num2;
+	std::cout << result;
 }
 
